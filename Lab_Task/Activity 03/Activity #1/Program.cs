8e04efcfe82174ccd7fad54fa51009e069489f4f
Muhammad Maaz <165182579+Muhammad-Maaz-0990@users.noreply.github.com>
2024-10-18 07:59:41 +0500

    Add files via upload

---
+++ Lab_Task/Activity 03/Activity #1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 10, b = 0;
+        double result = 0;
+        try
+        {
+            result = safeDivision(a, b);
+            Console.WriteLine($"{a} divided by {b} is {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Atempted Division by zero");
+        }
+    }
+
+    private static double safeDivision(double m, double n)
+    {
+        if (n == 0)
+        {
+            throw new System.DivideByZeroException();
+        }
+        return m / n;
+    }
+}
