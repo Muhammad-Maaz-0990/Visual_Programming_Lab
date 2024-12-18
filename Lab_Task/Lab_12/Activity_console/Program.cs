16cc4bd3e9a9e70f81bbd3cf7e8546dc3992b1b4
Muhammad Maaz <165182579+Muhammad-Maaz-0990@users.noreply.github.com>
2024-12-18 13:40:33 +0500

    Add files via upload

---
+++ Lab_Task/Lab_12/Activity_console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Activity_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6 };
+
+            var numQuery = from num in numbers where (num%2)==0 select num;
+
+            foreach (var num in numQuery)
+            {
+                Console.Write("{0,1}",num);
+            }
+        }
+    };
+}
